314ed3f35269bb527b34ec5e9c74355326dbf43b
hyunjong <isc4614@naver.com>
2024-05-05 18:38:03 +0900

    process

--- kernel/20193037_hyunjong/process.c
+++ kernel/20193037_hyunjong/process.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_PROCESSES 10
+#define MAX_PROCESSES 4
+#define RUN_TIME 4
+#define TIME_SLICE 1
 
 typedef enum process_state {
     RUNNING,
@@ -20,10 +22,10 @@ typedef struct {
 
 task_struct process_table[MAX_PROCESSES];
 
-// Function prototypes
 void initialize_process_table();
 int create_process(int memory_size);
-void schedule();
+void RR_process();
+void IRQ_process();
 void terminate_process(int pid);
 void display_processes();
 
@@ -35,6 +37,40 @@ int process() {
     schedule();
     terminate_process(pid1);
     schedule();
+    display_processes();
+
+    return 0;
+}
+
+int process_RR() {
+    initialize_process_table();
+    create_process(1024);  // 프로세스 1 생성
+    create_process(2048);  // 프로세스 2 생성
+    create_process(512);   // 프로세스 3 생성
+    create_process(256);   // 프로세스 4 생성
+    
+    RR_process();
+    terminate_process(pid1);
+    terminate_process(pid2);
+    terminate_process(pid3);
+    terminate_process(pid4);
+    display_processes();
+
+    return 0;
+}
+
+int process_IRQ() {
+    initialize_process_table();
+    create_process(1024);  // 프로세스 1 생성
+    create_process(2048);  // 프로세스 2 생성
+    create_process(512);   // 프로세스 3 생성
+    create_process(256);   // 프로세스 4 생성
+    
+    IRQ_process();
+    terminate_process(pid1);
+    terminate_process(pid2);
+    terminate_process(pid3);
+    terminate_process(pid4);
     display_processes();
 
     return 0;
@@ -69,6 +105,45 @@ void schedule() {
             process_table[i].state = RUNNING;
             printf("Process %d is running.\n", i);
         }
+    }
+}
+
+void RR_process() {
+    int time = 0;
+    while (time < RUN_TIME) {
+        for (int i = 0; i < MAX_PROCESSES; i++) {
+            if (process_table[i].state != TERMINATED) {
+                process_table[i].state = RUNNING;
+                printf("Process %d is running.\n", i);
+                sleep(TIME_SLICE);  // 각 프로세스 실행 시간
+                process_table[i].state = WAITING;
+                printf("Process %d is now waiting.\n", i);
+            }
+        }
+        time += TIME_SLICE;  // 각 프로세스의 실행 시간 추가
+    }
+}
+
+void IRQ_process() {
+    int elapsed_time = 0;
+    while (elapsed_time < RUN_TIME) {
+        for (int i = 0; i < MAX_PROCESSES; i++) {
+            if (process_table[i].state != TERMINATED) {
+                process_table[i].state = RUNNING;
+                printf("Process %d is running.\n", i);
+                sleep(1);  // 각 프로세스 실행 시간
+                if (i == 1) {  // 프로세스 2 실행 중 인터럽트 발생
+                    printf("Interrupt by Process 4.\n");
+                    process_table[3].state = RUNNING;  // 프로세스 4 실행
+                    printf("Process 4 is running.\n");
+                    sleep(1);
+                    process_table[3].state = WAITING;
+                }
+                process_table[i].state = WAITING;
+                printf("Process %d is now waiting.\n", i);
+            }
+        }
+        elapsed_time += 2;  // 각 프로세스 실행 + 인터럽트 처리 시간
     }
 }
 
