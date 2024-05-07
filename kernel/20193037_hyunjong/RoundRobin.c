67d415fb4bd877e538a7fcb05775e790b619a4d8
hyunjong <isc4614@naver.com>
2024-05-08 01:31:08 +0900

    modify

--- kernel/20193037_hyunjong/RoundRobin.c
+++ kernel/20193037_hyunjong/RoundRobin.c
@@ -1,88 +1,72 @@
 #include <stdio.h>
 
+// 프로세스 상태를 나타내는 열거형(enum)
 typedef enum {
-    READY, RUNNING, WAITING, TERMINATED
+    READY,     // 준비 상태
+    RUNNING,   // 실행중 상태
+    WAITING,   // 대기 상태
+    TERMINATED // 완료 상태
 } State;
 
+// 프로세스 정보를 담는 구조체 정의
 typedef struct {
     int processId;
     int arrivalTime;
     int burstTime;
     int remainingTime;
-    State state;
+    State state;  // 프로세스의 현재 상태
 } Process;
 
-// 상태 이름을 문자열로 변환하는 함수
-const char* getStateName(State state) {
-    switch (state) {
-        case READY:
-            return "READY";
-        case RUNNING:
-            return "RUNNING";
-        case WAITING:
-            return "WAITING";
-        case TERMINATED:
-            return "TERMINATED";
-        default:
-            return "UNKNOWN";
-    }
-}
-
-void printProcessState(Process p) {
-    printf("Process[%d] %s - Remaining Time: %d\n", p.processId, getStateName(p.state), p.remainingTime);
-}
-
 int main() {
-    int i, limit, total = 0, time_quantum;
+    int i, limit, total = 0, counter = 0, time_quantum;
     int wait_time = 0, turnaround_time = 0;
     float average_wait_time, average_turnaround_time;
-    printf("Enter Total Number of Processes: ");
+    printf("Enter Total Number of Processes:\t");
     scanf("%d", &limit);
-    Process processes[limit];
+    Process processes[limit]; // 프로세스 배열 생성
 
     for(i = 0; i < limit; i++) {
         printf("\nEnter Details of Process[%d]\n", i + 1);
-        printf("Arrival Time: ");
+        printf("Arrival Time:\t");
         scanf("%d", &processes[i].arrivalTime);
-        printf("Burst Time: ");
+        printf("Burst Time:\t");
         scanf("%d", &processes[i].burstTime);
         processes[i].remainingTime = processes[i].burstTime;
         processes[i].processId = i + 1;
-        processes[i].state = READY;
-        printProcessState(processes[i]);  // 초기 상태 출력
+        processes[i].state = READY; // 초기 상태를 READY로 설정
     }
 
-    printf("\nEnter Time Quantum: ");
+    printf("\nEnter Time Quantum:\t");
     scanf("%d", &time_quantum);
     printf("\nProcess ID\tBurst Time\tTurnaround Time\tWaiting Time\tState\n");
 
     int remaining_processes = limit;
 
     for(total = 0, i = 0; remaining_processes != 0;) {
-        if(processes[i].state != TERMINATED && processes[i].state != RUNNING) {
-            processes[i].state = RUNNING;
-            printProcessState(processes[i]);  // 상태 변경시 출력
+        if(processes[i].state != TERMINATED) {
+            processes[i].state = RUNNING; // 프로세스 상태를 RUNNING으로 설정
         }
         if(processes[i].remainingTime <= time_quantum && processes[i].remainingTime > 0) {
             total += processes[i].remainingTime;
             processes[i].remainingTime = 0;
-            processes[i].state = TERMINATED;
-            printProcessState(processes[i]);  // 상태 변경시 출력
-            remaining_processes--;
-            printf("\nProcess[%d]\t%d\t\t%d\t\t%d\t\tTERMINATED", processes[i].processId, processes[i].burstTime, total - processes[i].arrivalTime, total - processes[i].arrivalTime - processes[i].burstTime);
-            wait_time += total - processes[i].arrivalTime - processes[i].burstTime;
-            turnaround_time += total - processes[i].arrivalTime;
+            counter = 1;
+            processes[i].state = TERMINATED; // 프로세스 완료
         } else if(processes[i].remainingTime > 0) {
             processes[i].remainingTime -= time_quantum;
             total += time_quantum;
-            if (processes[i].remainingTime > 0) {
-                processes[i].state = READY;
-                printProcessState(processes[i]);  // 상태 변경시 출력
-            }
+        }
+        if(processes[i].state == TERMINATED && counter == 1) {
+            remaining_processes--;
+            printf("\nProcess[%d]\t%d\t\t%d\t\t%d\t\t%s", processes[i].processId, processes[i].burstTime, total - processes[i].arrivalTime, total - processes[i].arrivalTime - processes[i].burstTime, "TERMINATED");
+            wait_time += total - processes[i].arrivalTime - processes[i].burstTime;
+            turnaround_time += total - processes[i].arrivalTime;
+            counter = 0;
+        } else {
+            processes[i].state = READY; // 프로세스 상태를 READY로 재설정
         }
         if(i == limit - 1) {
             i = 0;
-        } else if(processes[i + 1].arrivalTime <= total) {
+        } else if(arrival_time[i + 1] <= total) {
             i++;
         } else {
             i = 0;
