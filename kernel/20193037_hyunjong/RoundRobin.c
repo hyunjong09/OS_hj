7fc9290f8d081ff1f0f72d68131491ac960ecdf8
hyunjong <isc4614@naver.com>
2024-05-08 02:01:57 +0900

    modify

--- kernel/20193037_hyunjong/RoundRobin.c
+++ kernel/20193037_hyunjong/RoundRobin.c
@@ -17,16 +17,16 @@ int RoundRobin() {
     Process processes[limit]; // 프로세스 배열 생성
 
     for(i = 0; i < limit; i++) {
-        printf("nEnter Details of Process[%d]\n", i + 1);
-        printf("Arrival Time:\t");
+        printf("\nEnter Details of Process[%d]\n", i + 1);
+        printf("Arrival Time: ");
         scanf("%d", &processes[i].arrivalTime);
-        printf("Burst Time:\t");
+        printf("Burst Time: ");
         scanf("%d", &processes[i].burstTime);
         processes[i].remainingTime = processes[i].burstTime; // 남은 실행 시간 초기화
         processes[i].processId = i + 1; // 프로세스 ID 설정
     }
 
-    printf("nEnter Time Quantum:\t");
+    printf("\nEnter Time Quantum: ");
     scanf("%d", &time_quantum);
     printf("\nProcess ID\tBurst Time\tTurnaround Time\tWaiting Time\n");
 
@@ -34,24 +34,27 @@ int RoundRobin() {
 
     // 모든 프로세스가 완료될 때까지 반복
     for(total = 0, i = 0; remaining_processes != 0;) {
-        if(processes[i].remainingTime <= time_quantum && processes[i].remainingTime > 0) {
-            total += processes[i].remainingTime;
-            processes[i].remainingTime = 0;
-            counter = 1;
-        } else if(processes[i].remainingTime > 0) {
-            processes[i].remainingTime -= time_quantum;
-            total += time_quantum;
-        }
-        if(processes[i].remainingTime == 0 && counter == 1) {
-            remaining_processes--;
-            printf("\nProcess[%d]\t\t%d\t\t%d\t\t%d", processes[i].processId, processes[i].burstTime, total - processes[i].arrivalTime, total - processes[i].arrivalTime - processes[i].burstTime);
-            wait_time += total - processes[i].arrivalTime - processes[i].burstTime;
-            turnaround_time += total - processes[i].arrivalTime;
-            counter = 0;
+        if(processes[i].remainingTime > 0) {
+            printf("Current running process: Process[%d]\n", processes[i].processId);
+            if(processes[i].remainingTime <= time_quantum) {
+                total += processes[i].remainingTime;
+                processes[i].remainingTime = 0;
+                counter = 1;
+            } else {
+                processes[i].remainingTime -= time_quantum;
+                total += time_quantum;
+            }
+            if(processes[i].remainingTime == 0 && counter == 1) {
+                remaining_processes--;
+                printf("\nProcess[%d]\t\t%d\t\t%d\t\t%d", processes[i].processId, processes[i].burstTime, total - processes[i].arrivalTime, total - processes[i].arrivalTime - processes[i].burstTime);
+                wait_time += total - processes[i].arrivalTime - processes[i].burstTime;
+                turnaround_time += total - processes[i].arrivalTime;
+                counter = 0;
+            }
         }
         if(i == limit - 1) {
             i = 0;
-        } else if(processes[i + 1].arrivalTime <= total) {
+        } else if((i + 1 < limit) && processes[i + 1].arrivalTime <= total) {
             i++;
         } else {
             i = 0;
@@ -60,7 +63,7 @@ int RoundRobin() {
 
     average_wait_time = wait_time * 1.0 / limit;
     average_turnaround_time = turnaround_time * 1.0 / limit;
-    printf("\n\nAverage Waiting Time:\t%f", average_wait_time);
-    printf("\nAverage Turnaround Time:\t%f\n", average_turnaround_time);
+    printf("\n\nAverage Waiting Time:\t%.3f", average_wait_time);
+    printf("\nAverage Turnaround Time:\t%.3f\n", average_turnaround_time);
     return 0;
 }
